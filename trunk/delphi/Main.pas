6735dc50e9850e8ac95df0319f29e0f931b5e0a1
albertorcf <albertorcf@d6029cc4-9a24-11dd-a26a-893737b22a74>
2008-10-17 21:33:28 +0000

    simple player

--- trunk/delphi/Main.pas
+++ trunk/delphi/Main.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Menus, ZConnection;
+  Dialogs, Menus, ZConnection, MPlayer, StdCtrls, Buttons, ComCtrls, ExtCtrls;
 
 type
   TfrmMain = class(TForm)
@@ -13,9 +13,19 @@ type
     Folders1: TMenuItem;
     ZConnection1: TZConnection;
     UpdateFolders1: TMenuItem;
+    MediaPlayer1: TMediaPlayer;
+    ListBox1: TListBox;
+    SpeedButton1: TSpeedButton;
+    OpenDialog1: TOpenDialog;
+    TrackBar1: TTrackBar;
+    Timer1: TTimer;
     procedure FormCreate(Sender: TObject);
     procedure Folders1Click(Sender: TObject);
     procedure UpdateFolders1Click(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure ListBox1DblClick(Sender: TObject);
+    procedure MediaPlayer1Notify(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     { Private declarations }
   public
@@ -37,6 +47,61 @@ begin
   Left := 0;
   ZConnection1.Database := ExtractFilePath(Application.ExeName) + 'mp3.db';
   ZConnection1.Connected := True;
+end;
+
+procedure TfrmMain.ListBox1DblClick(Sender: TObject);
+begin
+  Timer1.Enabled := False;
+  MediaPlayer1.Close;
+  MediaPlayer1.FileName := ListBox1.Items[ListBox1.ItemIndex];
+  MediaPlayer1.Open;
+  TrackBar1.Max := MediaPlayer1.Length;
+  TrackBar1.Position := 0;
+  MediaPlayer1.Play;
+  Timer1.Enabled := True;
+end;
+
+procedure TfrmMain.MediaPlayer1Notify(Sender: TObject);
+begin
+  case MediaPlayer1.NotifyValue of
+    nvSuccessful: begin
+      Timer1.Enabled := False;
+      TrackBar1.Max := 0;
+      if ListBox1.ItemIndex < ListBox1.Count - 1 then begin
+        ListBox1.ItemIndex := ListBox1.ItemIndex + 1;
+        ListBox1DblClick(nil);
+      end;
+    end;
+    nvSuperseded: begin
+      Timer1.Enabled := False;
+      TrackBar1.Max := 0;
+    end;
+    nvAborted: begin
+      Timer1.Enabled := False;
+      TrackBar1.Max := 0;
+    end;
+    nvFailure: begin
+      Timer1.Enabled := False;
+      TrackBar1.Max := 0;
+    end;
+  end;
+end;
+
+procedure TfrmMain.SpeedButton1Click(Sender: TObject);
+var
+  I: Integer;
+begin
+  if OpenDialog1.Execute then begin
+    for I := 0 to OpenDialog1.Files.Count - 1 do begin
+      ListBox1.Items.Add(OpenDialog1.Files[I]);
+    end;
+  end;
+end;
+
+procedure TfrmMain.Timer1Timer(Sender: TObject);
+begin
+  if TrackBar1.Max > 0 then
+    TrackBar1.Position := MediaPlayer1.Position;
 end;
 
 procedure TfrmMain.Folders1Click(Sender: TObject);
