21c0ac4f586681ff8f12916962bdfed7fd48b7c9
albertorcf <albertorcf@d6029cc4-9a24-11dd-a26a-893737b22a74>
2008-10-24 19:45:43 +0000

    new db schema

--- trunk/db/script001.sql
+++ trunk/db/script001.sql
@@ -4,20 +4,14 @@ create table artist(
 );
 create unique index artist_name_index on artist(name);
 
-create table album(
-  id        integer primary key,
-  id_artist integer,
-  name      varchar(50),
-  year      integer,
-  rating    integer,
-  foreign key (id_artist) references artist(id)
-);
-
 create table file(
   id        integer primary key,
   path      varchar(400),
+  title     varchar(50),
   id_artist integer,
-  id_album  integer
+  album     varchar(50),
+  year      integer,
+  track     integer
 );
 create unique index file_path_index on file(path);
 
@@ -25,10 +19,12 @@ create table playlist(
   id   integer primary key,
   name varchar(50)
 );
+insert into playlist (id, name) values (1, '__last__');
 
 create table file_playlist(
   id_file     integer,
   id_playlist integer,
+  ord         integer,
   primary key (id_file, id_playlist),
   foreign key (id_file) references file(id),
   foreign key (id_playlist) references playlist(id)
